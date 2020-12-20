db4d8efee2314bea653df11223b409b94e49c1f0
SangA-Lee <57246079+SangA-Lee@users.noreply.github.com>
2020-12-20 23:12:01 +0900

    Create selection.py

---
+++ Sort/selection/selection.py
@@ -0,0 +1,23 @@
+def bubble_min(datas):
+    size = len(datas)
+    for i in range(0,size):
+        min = i
+        for j in range(i,size):
+            if datas[min] > datas[j]:
+                min = j
+                
+        temp = datas[i]
+        datas[i] = datas[min]
+        datas[min] = datas[i]
+
+def bubble_max(datas):
+    size = len(datas)
+    for i in range(0,size):
+        max = 0
+        for j in range(0,size-i):
+            if datas[max] < datas[j]:
+                max = j
+                
+        temp = datas[size-i-1]
+        datas[size-i-1] = datas[max]
+        datas[max] = datas[size-i-1]
