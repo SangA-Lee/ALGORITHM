c845e030be217f7a7f4dce2f0e49336f09d12036
SangA-Lee <57246079+SangA-Lee@users.noreply.github.com>
2020-12-20 22:01:54 +0900

    Create bubble.c

---
+++ Sort/bubble/bubble.c
@@ -0,0 +1,35 @@
+int* bubble(int datas[], int _size)
+{
+    int size = _size - 1;
+    for(int i = 0 ; i < size ; i++)
+    {
+        for(int j = 0 ; j < size - i ; j++)
+        {
+            if(datas[j] > datas[j+1])
+            {
+                int temp = datas[j];
+                datas[j] = datas[j+1];
+                datas[j+1] = temp;
+            }
+        }
+    }
+    return &datas[0];
+}
+
+void bubble2(int* datas, int _size)
+{
+    int size = _size - 1;
+    for(int i = 0 ; i < size ; i++)
+    {
+        for(int j = 0 ; j < size - i ; j++)
+        {
+            if(datas[j] > datas[j+1])
+            {
+                int temp = datas[j];
+                datas[j] = datas[j+1];
+                datas[j+1] = temp;
+            }
+        }
+    }
+    return;
+}
