1bf2e5920b1dca13c9dc4eab95714a362e555c74
SangA-Lee <57246079+SangA-Lee@users.noreply.github.com>
2020-12-25 23:14:09 +0900

    Create insertion.hpp

---
+++ Sort/insertion/insertion.hpp
@@ -0,0 +1,22 @@
+int insertion(vector<int> &datas)
+{
+    int pass = 0;
+    int cur;
+    int size = (int)(datas.size());
+    
+    for(int i = 1 ; i < size ; i++)
+    {
+        cur = datas[i];
+        int j = i - 1;
+        while(j >= 0 && datas[j] > cur)
+        {
+            datas[j+1] = datas[j];
+            j--;
+            
+            pass++;
+        }
+        datas[j+1] = cur;
+    }
+    
+    return pass;
+}
